838bcbdb5a4f78100914d4a2b3d3a5802f2f17ea
Walter Wong <32001362+Walt280@users.noreply.github.com>
2020-10-23 22:50:56 -0400

    Add project files

---
+++ PrimeFinderCSharp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFinderCSharp
+{
+    /// <summary>
+    /// A program to find all prime integers between 1 and N.
+    /// </summary>
+    /// <remarks>
+    /// The basic algorithm works like this:
+    /// for each number (num) from 2 to n:
+    ///     find the modulo of num and all the prime numbers less than the square root of n
+    ///     if none of the prime numbers modulo with num to equal zero, than num is prime
+    ///     else num is not prime
+    /// this is possible because composite numbers can be seen as prime numbers multiplied together
+    /// therefore we only need to divide by prime numbers to check for primality, because all non-prime 
+    /// numbers can be factorized into prime numbers.
+    /// this algorithm is effectively more efficient repeated division.
+    /// </remarks>
+    class Program
+    {
+        static void Main()
+        {
+            // Read input from the user.
+            Console.WriteLine("This is a program to find all primes from 1 to N.");
+            Console.WriteLine("Please enter a number: ");
+
+            var validInput = false;
+            long n = 0;
+            while(!validInput)
+            {
+                var input = Console.ReadLine();
+                try
+                {
+                    n = long.Parse(input);
+
+                    if (n < 1)
+                    {
+                        Console.WriteLine($"Invalid integer: \"{input}\"");
+                    }
+                    else
+                    {
+                        validInput = true;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Invalid input: \"{input}\"");
+                }
+            }
+
+            // Find all the prime numbers.
+            var primeList = new List<long>();
+            for (long i = 2; i <= n; i++)
+            {
+                // The square root is used to reduce the amount of division needed to do;
+                // factors repeat past the square root.
+                // Ex: sqrt(20) = 4.472 = 5
+                // 20 = 2 * 10
+                //    = 4 * 5
+                //    = 5 * 4
+                //    = 10 * 2
+                var sq = Math.Ceiling(Math.Sqrt(i));
+                var isPrime = true;
+
+                for (int j = 0; j < primeList.Count && isPrime && primeList[j] <= sq; j++)
+                {
+                    if (i % primeList[j] == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primeList.Add(i);
+                }
+            }
+
+            // Print out all the found prime numbers.
+            Console.WriteLine($"Prime numbers from 1 to {n}:");
+            foreach (var prime in primeList)
+            {
+                Console.WriteLine(prime);
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimeFinderFSharp/Program.fs
@@ -0,0 +1,85 @@
+﻿(*
+ A program to find all prime integers between 1 and N.
+
+ The basic algorithm works like this:
+ for each number (num) from 2 to n:
+     find the modulo of num and all the prime numbers less than the square root of n
+     if none of the prime numbers modulo with num to equal zero, than num is prime
+     else num is not prime
+ this is possible because composite numbers can be seen as prime numbers multiplied together
+ therefore we only need to divide by prime numbers to check for primality, because all non-prime 
+ numbers can be factorized into prime numbers.
+ this algorithm is effectively more efficient repeated division.
+
+ Implementation notes:
+ I have tried to use as many functional features/patterns as possible to make this not a straight port 
+ of the c# version.
+*)
+
+open System
+
+(* Function to print out the list of primes. *)
+let rec printPrimeList list =
+    match list with
+    | head::tail -> 
+        printfn "%d" head
+        printPrimeList tail
+    | [] -> ()
+
+(* 
+ Function to check if number is prime, based on a list of primes.
+ numSqrt used to reduce amount of division done.
+*)
+let rec isPrime number numSqrt primeList = 
+    match primeList with
+    | head :: tail ->
+        if numSqrt < head then
+            true
+        else if number % head = 0L then
+            false
+        else
+            isPrime number numSqrt tail
+    | [] -> true
+
+(* Function to get a list of prime numbers. *)
+let rec getPrimes current upperBound primeList =
+    if current > upperBound then
+        primeList
+    else
+        let sq = current |> double |> sqrt |> ceil |> int64
+        let isCurrentPrime = isPrime current sq primeList
+        match isCurrentPrime with
+        | true -> getPrimes (current + 1L) upperBound (current::primeList)
+        | false -> getPrimes (current + 1L) upperBound primeList
+
+(* Function to read an int64 from stdin. *)
+let rec readLongFromStdin() = 
+    let v = Console.ReadLine()
+    try
+        let longVal =  v |> int64
+
+        if longVal < 1L then
+            printfn "Invalid integer: %d" longVal
+            readLongFromStdin()
+        else
+            Some longVal
+    with 
+    | :? System.FormatException -> 
+        printfn "Invalid input: \"%s\"" v
+        readLongFromStdin()
+    | _ -> None
+
+[<EntryPoint>]
+let main argv =
+    printfn "This is a program to find all primes from 1 to N."
+    printfn "Please enter a number: "
+    let value = readLongFromStdin()
+    match value with
+    | Some n ->
+        let primeList = getPrimes 2L n []
+        printfn "The prime numbers between 1 and %d are:" n
+        primeList |> List.rev |> printPrimeList
+        0
+    | None ->
+        printfn "An error has occured."
+        -1
